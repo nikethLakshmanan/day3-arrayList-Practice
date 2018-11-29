23484f39d8abf83cfb802f97b6ae52a3c68cfdc9
2001SameerSharma <45395094+2001SameerSharma@users.noreply.github.com>
2018-11-29 10:16:16 -0500

    Revert "Added constructInt"
    This reverts commit 50753f07d2bdbaf960abbcc846631ea82e53b461.

--- gitHubArrayListPractice.java
+++ gitHubArrayListPractice.java
@@ -1,25 +1,7 @@
 import java.util.ArrayList;
 public class gitHubArrayListPractice{
-
-	public static int constructInt(ArrayList<Integer> list){
-		int [] number = new int[list.size()];
-		int num = 0;
-		for(int i=0; i<list.size(); i++){
-			number[i] = list.get(i);
-		}
-		for(int i=0; i<list.size(); i++){
-			num += number[i];
-			num*=10;
-		if(i==list.size()-1)
-			num/=10;
-		}
-		return num;
-	}
-
-
 	public static void main(String[]args){
-		System.out.println(convertToArrayList(123));
-		System.out.println(constructInt(convertToArrayList(123)));
+
 
 	}
 }
