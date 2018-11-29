cac35cf5a28ad495b171236f7d13cb863cba3a87
nikethLakshmanan <45395176+nikethLakshmanan@users.noreply.github.com>
2018-11-29 10:03:57 -0500

    added method convert
    made a new method

--- gitHubArrayListPractice.java
+++ gitHubArrayListPractice.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 public class gitHubArrayListPractice{
 	public static void main(String[]args){
 
-
+	}
+		public static ArrayList<Integer> convertToArrayList(int a){
+			ArrayList<Integer> list = new ArrayList<>();
+			return list;
 	}
 }
